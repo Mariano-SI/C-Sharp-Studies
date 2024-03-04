c8d5b7c9523f78124b5522da125c32d82b68654b
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-03-04 20:29:04 -0300

    execicio strings

--- Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
+++ Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7491ada9fd1f5caef81239e8c1153c6b25da63ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4ddabdd0f549d025e6d703c5516721b3f6fdd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
