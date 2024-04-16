37c3dac1afa6148ccc6042aa4e3f15ce69d60e95
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-04-16 07:22:18 -0300

    exercicio proposto excecoes

---
+++ Exercicios/06-ExercicioPropostoExcecoes/06-ExercicioPropostoExcecoes/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using _06_ExercicioPropostoExcecoes.Entities.Exceptions;
+
+
+namespace _06_ExercicioPropostoExcecoes.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit; 
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+               throw new DomainException("It is not possible to deposit a negative amount");
+            }
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }else if (Balance <= 0)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            else if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ Exercicios/06-ExercicioPropostoExcecoes/06-ExercicioPropostoExcecoes/Program.cs
@@ -0,0 +1,38 @@
+﻿using _06_ExercicioPropostoExcecoes.Entities;
+using _06_ExercicioPropostoExcecoes.Entities.Exceptions;
+using System.Globalization;
+
+namespace _06_ExercicioPropostoExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+            Console.Write("Enter amount for withdraw: ");
+            double withdrawAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.Withdraw(withdrawAmount);
+
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
