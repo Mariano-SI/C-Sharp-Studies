a34cc959b2c093bbc289fff67cfc3c5345e5019e
Mariano Silva <76002553+Mariano-SI@users.noreply.github.com>
2024-05-07 08:53:21 -0300

    Exercicio resolvido: conjuntos

---
+++ Exercicios/08-ExResolvidoConjuntos/08-ExResolvidoConjuntos/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ExResolvidoConjuntos.Entities
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public DateTime AcessTime { get; set; }
+
+        public User(string userName, DateTime acessTime)
+        {
+            UserName = userName;
+            AcessTime = acessTime;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is User))
+            {
+                throw new ArgumentException("Obj must be a User");
+            }
+
+            User other = obj as User;
+
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ Exercicios/08-ExResolvidoConjuntos/08-ExResolvidoConjuntos/Program.cs
@@ -0,0 +1,46 @@
+﻿using _08_ExResolvidoConjuntos.Entities;
+using System.IO;
+
+namespace _08_ExResolvidoConjuntos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DESCRICAO:
+
+            Um site de internet registra um log de acessos dos usuários. Um registro de log consiste no nome de usuário
+            e o instante em que ousuário acessou o site no padrão ISO 8601, separados por espaço,conforme exemplo. Fazer
+            um programa que leia o log de acessos apartir de um arquivo, e daí informe quantos usuários distintos acessaram 
+            o site.   
+            */
+
+            Console.Write("Enter file full path: "); // O arquivo se enontra na mesma pasta do projeto
+            string path = Console.ReadLine();
+
+            HashSet<User> users = new HashSet<User>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] data = line.Split(' ');
+                        string userName = data[0];
+                        DateTime acessTime = DateTime.Parse(data[1]);
+
+                        users.Add(new User(userName, acessTime));
+                    }
+                }
+
+                Console.Write("Total users: " + users.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
