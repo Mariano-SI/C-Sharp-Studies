4074284a726a95d86fb9ccee33e264b9bb60df89
Mariano Silva <76002553+Mariano-SI@users.noreply.github.com>
2024-05-11 11:11:23 -0300

    Introducao ao linq

---
+++ Cap17-ExpressoesLambdaDelegatesELINQ/Cap17-ExpressoesLambdaDelegatesELINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace Cap17_ExpressoesLambdaDelegatesELINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - especificar a fonte de dados
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            //2 - definir a consulta
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10); //O where serviu como um filter do JS e o Select um map;
+
+            // 3 - Executar a consulta
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
